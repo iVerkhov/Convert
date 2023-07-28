848b8005292385b6f6651e34be58b1a65a72a0b3
iVerkhov <vrkhv@iCloud.com>
2023-07-28 11:41:00 +0300

    Initial Commit

---
+++ Convert/ConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertApp.swift
+//  Convert
+//
+//  Created by Игорь Верхов on 28.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
